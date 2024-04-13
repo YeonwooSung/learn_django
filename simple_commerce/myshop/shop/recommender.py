757a18e6bf674f1741def28b66bbaa972b2b8d1c
YeonwooSung <neos960518@gmail.com>
2024-04-13 11:51:23 +0900

    refactor: indentation issues

--- simple_commerce/myshop/shop/recommender.py
+++ simple_commerce/myshop/shop/recommender.py
@@ -20,21 +20,25 @@ class Recommender:
                 # get the other products bought with each product
                 if product_id != with_id:
                     # increment score for product purchased together
-                    r.zincrby(self.get_product_key(product_id),
-                              1,
-                              with_id)
+                    r.zincrby(
+                        self.get_product_key(product_id),
+                        1,
+                        with_id
+                    )
 
     def suggest_products_for(self, products, max_results=6):
         product_ids = [p.id for p in products]
         if len(products) == 1:
             # only 1 product
-            suggestions = r.zrange(
-                             self.get_product_key(product_ids[0]),
-                             0, -1, desc=True)[:max_results]
+            product_key = self.get_product_key(product_ids[0])
+            # get the products bought together with the given product
+            suggestions = r.zrange(product_key, 0, -1, desc=True)[:max_results]
+
         else:
             # generate a temporary key
             flat_ids = ''.join([str(id) for id in product_ids])
             tmp_key = f'tmp_{flat_ids}'
+
             # multiple products, combine scores of all products
             # store the resulting sorted set in a temporary key
             keys = [self.get_product_key(id) for id in product_ids]
@@ -42,8 +46,8 @@ class Recommender:
             # remove ids for the products the recommendation is for
             r.zrem(tmp_key, *product_ids)
             # get the product ids by their score, descendant sort
-            suggestions = r.zrange(tmp_key, 0, -1,
-                                   desc=True)[:max_results]
+            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_results]
+
             # remove the temporary key
             r.delete(tmp_key)
         suggested_products_ids = [int(id) for id in suggestions]
